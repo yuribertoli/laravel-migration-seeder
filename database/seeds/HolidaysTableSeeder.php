1008fc5d2ec58c6ab7ea617237f372f039ad976d
yuribertoli <yuribertoli@yahoo.it>
2022-03-31 21:40:00 +0200

    faker

--- database/seeds/HolidaysTableSeeder.php
+++ database/seeds/HolidaysTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use Faker\Generator as Faker;
 use App\Holiday;
 use Illuminate\Database\Seeder;
 
@@ -10,23 +11,25 @@ class HolidaysTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        $holiday = new Holiday();
-        
-        $holiday->trip_title = 'Working on the cotton fields';
-        $holiday->reference_id= 'TG3521HJ';
-        $holiday->price = 2000;
-        $holiday->starting_date = '2023/01/01';
-        $holiday->description = 'Feel like a slave working in a cotton field!';
-        $holiday->destination_country = 'USA';
-        $holiday->departure_country = 'Italy';
-        $holiday->destination_city = 'Dallas';
-        $holiday->departure_city = 'Rome';
-        $holiday->type = 'Slavery';                
-        $holiday->trip_lenght = 14;            
-        $holiday->people = 1;
+        for ($i=0; $i<100; $i++){
 
-        $holiday->save();
+            $holiday = new Holiday();
+            $holiday->trip_title = $faker->sentence(4); //random sentence of 6 words
+            $holiday->reference_id= $faker->randomNumber(5, true) . $faker->hexColor(); //exactly (true) 5 random number + random hex color
+            $holiday->price = $faker->randomNumber(5, false);; //up to (false) 5 random number
+            $holiday->starting_date = $faker->dateTimeThisYear('+9 months'); //a date somewhere in this year, with an upper bound of +9 months
+            $holiday->description = $faker->sentence(20); //random sentence of 20 words
+            $holiday->destination_country = $faker->word(); 
+            $holiday->departure_country = $faker->word(); /* $faker->country(); //Generate a random country *//* $faker->countryISOAlpha3(); //Generate a random three-letter country code string */
+            $holiday->destination_city = $faker->city(); //Generate a random city
+            $holiday->departure_city = $faker->city(); //Generate a random city
+            $holiday->type = $faker->word();  //Generate a random word               
+            $holiday->trip_lenght = $faker->randomNumber(2, false);   //up to (false) 2 random number         
+            $holiday->people = $faker->randomNumber(2, false);  //up to (false) 2 random number
+            $holiday->save();
+        }
+    
     }
 }
